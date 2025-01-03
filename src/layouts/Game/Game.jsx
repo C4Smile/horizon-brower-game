b4c02fddeb79fe7982a9470ede2e0a24ddf1771a
Sito <sito8943@gmail.com>
2025-01-03 07:54:34 -0500

    working with actions

--- src/layouts/Game/Game.jsx
+++ src/layouts/Game/Game.jsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router-dom";
-import Resources from "./components/Resources";
+import Resources from "./components/Resources/Resources.jsx";
 
 const Game = () => {
   return (
     <>
-      <header className="fixed flex p-3 top-0 left-0 w-full justify-between">
+      <header className="flex p-3 w-full justify-between">
         <div></div>
         <Resources />
       </header>
       <Outlet />
+      <footer className="flex p-3 w-full justify-between">
+        <Actions />
+      </footer>
     </>
   );
 };
---
+++ src/layouts/Game/components/Actions/Action.jsx
@@ -0,0 +1,16 @@
+import Tippy from "@tippyjs/react";
+
+const Action = (props) => {
+
+  const { image, tooltip, name, aria, id } = props;
+
+  return (
+    <Tippy content={tooltip}>
+      <button id={id} type="button" aria-label={aria} className="flex flex-col items-center justify-center">
+        <img className="w-8 h-8 rounded-full object-cover" src={image}
+             alt={name} />
+      </button>
+    </Tippy>);
+};
+
+export default Action
