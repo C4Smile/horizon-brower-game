01ab22a7accff798824294631a1a0a58d7bac986
Sito <sito8943@gmail.com>
2025-01-10 08:36:05 -0500

    now useEnqueue can show notifications

--- src/layouts/Game/Buildings/actions/useEnqueue.jsx
+++ src/layouts/Game/Buildings/actions/useEnqueue.jsx
@@ -4,17 +4,21 @@ import { useTranslation } from "react-i18next";
 
 // icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHammer, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 // providers
 import { queryClient, useHorizonApiClient } from "../../../../providers/HorizonApiProvider";
+import { useNotification } from "../../../../providers/NotificationProvider";
 
 // utils
 import { ReactQueryKeys } from "../../../../utils/queryKeys";
 
 export const useEnqueueAction = (props) => {
+  const { t } = useTranslation();
 
   const { userId, buildingAction, icon, aria, tooltip } = props;
+
+  const { setNotification } = useNotification();
 
   const horizonApiClient = useHorizonApiClient();
 
@@ -23,21 +27,21 @@ export const useEnqueueAction = (props) => {
       horizonApiClient.Building.enqueue({
         playerId: userId,
         buildingId,
-        action: buildingAction
+        action: buildingAction,
       }),
     onSuccess: async (result) => {
-      const { error, status } = result;
-
-      if (status !== 201) {
-        console.error(error);
+      const { error } = result;
+      if (error) {
+        console.log(error);
+        setNotification(t(`_game:buildings.notifications.${error.status}`, {}, "error"));
       } else {
         await queryClient.invalidateQueries([ReactQueryKeys.Buildings, userId]);
-        console.info(200);
+        setNotification(t("_game:buildings.notifications.201"));
       }
     },
     onError: async (error) => {
       console.error(error);
-    }
+    },
   });
 
   const [itemId, setItemId] = useState(null);
@@ -53,16 +57,13 @@ export const useEnqueueAction = (props) => {
         },
         hidden,
         icon: (
-          <FontAwesomeIcon
-            className="text-xl text-light-primary"
-            icon={isLoading ? faSpinner : icon}
-          />
+          <FontAwesomeIcon className="text-xl text-light-primary" icon={isLoading ? faSpinner : icon} />
         ),
         tooltip,
-        aria
+        aria,
       };
     },
-    [itemId, mutateFn, buildingAction, icon, tooltip, aria]
+    [itemId, mutateFn, buildingAction, icon, tooltip, aria],
   );
 
   return { action };
